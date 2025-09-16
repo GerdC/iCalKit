e3ee6bf60e73903489deab750a81bc9e7d002985
Gerd Castan <gerd@gerdcastan.de>
2025-09-16 23:29:00 +0200

    make it run on Xcode 26 again: no semantic code change, updated Package.swift, requiring minimum iOS 12 and corresponding versions.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,28 @@
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
-    name: "iCalKit"
+    name: "iCalKit",
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v10_14),
+        .watchOS(.v5),
+        .tvOS(.v12)
+    ]
+    products: [
+        .library(
+            name: "iCalKit",
+            targets: ["iCalKit"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "iCalKit"
+            // resources: [.process("Resources")]
+        ),
+        .testTarget(
+            name: "iCalKitTests",
+            dependencies: ["iCalKit"]
+        )
+    ]
 )
